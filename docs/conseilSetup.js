5a4bcd617352ebea1eea35d87a50fc2969fdce60
PrathamRawat <prathamrawat815@gmail.com>
2020-04-15 11:07:31 -0400

    Add function to generate link

--- docs/conseilSetup.js
+++ docs/conseilSetup.js
@@ -1,1 +1,6 @@
 const conseilServer = { url: 'https://conseil-prod.cryptonomic-infra.tech:443', apiKey: 'f86ab59d-d2ea-443b-98e2-6c0785e3de8c', network: 'mainnet' };
+
+const shareReport = (network, entity, query) => {
+    const encodedUrl = atob(JSON.stringify(query));
+    return `https://arronax.io/tezos/${network}/${entity}/query/${encodedUrl}`;
+};
