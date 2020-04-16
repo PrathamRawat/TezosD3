596ec7bf3e929c024a6ea67b0d1b911cf1a925cf
PrathamRawat <prathamrawat815@gmail.com>
2020-04-16 13:26:28 -0400

    Fix scaling issue, and add daily originations

--- docs/dailyOriginations.js
+++ docs/dailyOriginations.js
@@ -1,9 +1,9 @@
 
-let apdQuery = async function(date) {
+let opdQuery = async function(date) {
     let query = conseiljs.ConseilQueryBuilder.blankQuery();
     query = conseiljs.ConseilQueryBuilder.addFields(query, 'kind');
     query = conseiljs.ConseilQueryBuilder.addFields(query, 'timestamp');
-    query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'kind', conseiljs.ConseilOperator.EQ, ['activate_account']);
+    query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'kind', conseiljs.ConseilOperator.EQ, ['origination']);
     query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'timestamp', conseiljs.ConseilOperator.BETWEEN, [date, date + (86400000 * 365)]);
     query = conseiljs.ConseilQueryBuilder.addOrdering(query, "timestamp", conseiljs.ConseilSortDirection.ASC);
     query = conseiljs.ConseilQueryBuilder.setLimit(query, 1000000000);
@@ -14,7 +14,7 @@ let apdQuery = async function(date) {
 
     label = [];
     times = [];
-    activations = [];
+    originations = [];
     data = [];
 
     now = date + (86400000 * 365);
@@ -28,7 +28,7 @@ let apdQuery = async function(date) {
         value.setHours(0, 0, 0, 0)
         label.push(value);
         times.push(value.getTime());
-        activations.push(0)
+        originations.push(0)
     }
 
     // console.log(label);
@@ -36,19 +36,19 @@ let apdQuery = async function(date) {
     for(var r = 0; r < result.length; r++) {
         for(var t = label.length - 1; t > 0; t--) {
             if(parseInt(result[r].timestamp) > parseInt(label[t].getTime())) {
-                activations[t] += 1;
+                originations[t] += 1;
                 break;
             }
         }
     }  
 
-    for(var x = 0; x < activations.length; x++) {
-        data.push({date : label[x].getTime(), values : parseInt(activations[x])});
+    for(var x = 0; x < originations.length; x++) {
+        data.push({date : label[x].getTime(), values : parseInt(originations[x])});
     }
 
     console.log(data);
     
-    activations.pop()
+    originations.pop()
     times.pop()
     data.pop()
     label.pop()
@@ -56,18 +56,18 @@ let apdQuery = async function(date) {
     height = 500;
 
     var y = d3.scaleLinear()
-      .domain([0, d3.max(activations) + 5])
+      .domain([0, d3.max(originations) + 5])
       .range([ 25, height ]);
 
     var yScale = d3.scaleLinear()
-      .domain([0, d3.max(activations) + 5])
+      .domain([0, d3.max(originations) + 5])
       .range([ -25, -height]);
 
     var x = d3.scaleTime()
         .domain(d3.extent(data, function(d) { return d.date; }))
         .range([ 0, 1200 ]);
 
-    const svg = d3.select("#activationsPerDay")
+    const svg = d3.select("#originationsPerDay")
         .attr("height", height)
         .attr("width", x.range()[1] + 25)
         .attr("font-family", "sans-serif")
@@ -96,22 +96,22 @@ let apdQuery = async function(date) {
 
     svg.append("g").attr("transform", "translate(40, 500)").style("color", "black").call(yAxis);
 
-    pointer = svg.append("circle")
+    highlight = svg.append("circle")
 
-    show = d3.select("#apdLabel");
+    desc = d3.select("#opdLabel");
     svg.on("mousemove", function() {
         date = x.invert(d3.event.clientX - d3.event.target.getBoundingClientRect().left) 
         date.setHours(0, 0, 0, 0);
 
         d = times.indexOf(date.getTime());
 
-        pointer
+        highlight
             .attr("cx", x(date.getTime()))
-            .attr("cy", 500 - y(activations[d - 12]))
+            .attr("cy", 500 - y(originations[d - 12]))
             .attr("r", 5)
             .attr("fill", "purple")
 
-        show.html((activations[d - 12]) + " Activations on " + date.toDateString())
+        desc.html((originations[d - 12]) + " originations on " + date.toDateString())
     });
 
     return result;                                              
@@ -123,4 +123,4 @@ now.setMinutes(0, 0, 0);
 
 now = now.getTime();
 
-apdQuery(now - (86400000 * 365));
+opdQuery(now - (86400000 * 365));
