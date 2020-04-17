72e17449bad33b07eff79a5b5452821933efbf84
PrathamRawat <prathamrawat815@gmail.com>
2020-04-17 16:17:50 -0400

    Add chart generator to bakers page

--- docs/topBakers.js
+++ docs/topBakers.js
@@ -13,73 +13,11 @@ let bakerQuery = async function(limit, date) {
 
     d3.select("#topBakersLink").attr("href", shareReport("mainnet", "blocks", query))
 
-    console.log(result);
+    graphSVG = d3.select("#topBakers");
 
-    data = result.map(function(x) {return x.count_hash});
-    accounts = result.map(function(x) {return x.baker});
-    
-    console.log(data);
-    
-    // bakerCount = data.map(function(x) {return x[1]});
-    
+    graphAxis = d3.select("#topBakersAxis");
 
-    y = d3.scaleLinear()
-        .domain([0, data[0]])
-        .range([0, height])
-
-    yScale = d3.scaleLinear()
-        .domain([0, data[0]])
-        .range([0, -height])
-
-    x = d3.scaleBand()
-        .domain(d3.range(data.length))
-        .range([0, 25 * data.length]);
-
-    const svg = d3.select("#topBakers")
-        .attr("height", height)
-        .attr("width", x.range()[1])
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "10")
-        .attr("text-anchor", "end");
-
-    svg.selectAll("*").remove();
-
-    const bar = svg.selectAll("g")
-        .data(data)
-        .join("g")
-        .attr("transform", (d, i) => `translate(${x(i) + 100}, ${500 - y(d)})`);
-
-    bar.append("rect")
-        .attr("fill", "purple")
-        .attr("width", x.bandwidth() - 1)
-        .attr("height", 0);
-
-    bar.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("height", y);
-
-    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
-
-    bar.on("mousemove", function(d, i){
-        tooltip
-        .style("left", d3.event.pageX - 50 + "px")
-        .style("top", d3.event.pageY - 70 + "px")
-        .style("display", "inline-block")
-        .html((d) + " Blocks<br>" + (accounts[i]));
-    })
-        .on("mouseout", function(d){ tooltip.style("display", "none");});
-
-    const yAxis = d3.axisLeft()
-                    .scale(yScale);
-
-    const axisSVG = d3.select("#topBakersAxis")
-        .attr("height", height)
-        .attr("width", 60);
-
-    axisSVG.selectAll("*").remove();
-
-    axisSVG.append("g").attr("transform", "translate(60, 500)").style("color", "black").call(yAxis);
+    seperateAxisDynamicBarChartGenerator(500, 25, graphSVG, graphAxis, result, "baker", "count_hash");
 
     return result;                                              
 }
--- docs/topStakers.js
+++ docs/topStakers.js
@@ -9,71 +9,11 @@ let stakeQuery = async function(limit) {
 
     d3.select("#topStakersLink").attr("href", shareReport("mainnet", "delegates", query))
 
+    graphSVG = d3.select("#topStakers");
 
-    // console.log(result);
+    graphAxis = d3.select("#topStakersAxis");
 
-    data = result.map(function(x) {return x.staking_balance / 1000000});
-    accounts = result.map(function(x) {return x.pkh});
-
-    // console.log(accounts);
-
-    height = 500;
-    
-    console.log(data);
-
-    y = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, height])
-    
-    yScale = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, -height])
-
-    x = d3.scaleBand()
-        .domain(d3.range(data.length))
-        .range([0, 25 * data.length]);
-
-    const svg = d3.select("#topStakers")
-        .attr("height", height)
-        .attr("width", x.range()[1])
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "10")
-        .attr("text-anchor", "end");
-    
-    const bar = svg.selectAll("g")
-        .data(data)
-        .join("g")
-          .attr("transform", (d, i) => `translate(${x(i) + 100}, ${500 - y(d)})`);
-
-    bar.append("rect")
-        .attr("fill", "purple")
-        .attr("width", x.bandwidth() - 1)
-        .attr("height", 0);
-  
-    bar.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("height", y);
-
-    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
-
-    bar.on("mousemove", function(d, i){
-        tooltip
-        .style("left", d3.event.pageX - 50 + "px")
-        .style("top", d3.event.pageY - 70 + "px")
-        .style("display", "inline-block")
-        .html((d) + " XTz<br>" + (accounts[i]));
-    })
-        .on("mouseout", function(d){ tooltip.style("display", "none");});
-
-    const yAxis = d3.axisLeft()
-                    .scale(yScale);
-    
-    const axisSVG = d3.select("#topStakersAxis")
-        .attr("height", height)
-        .attr("width", 60);
-
-    axisSVG.append("g").attr("transform", "translate(60, 500)").style("color", "black").call(yAxis);
+    seperateAxisDynamicBarChartGenerator(500, 25, graphSVG, graphAxis, result, "pkh", "staking_balance");
 
     return result;
 }
