295959c3d9e64495e2322e58677c1594ecdb3947
PrathamRawat <prathamrawat815@gmail.com>
2020-04-29 16:17:38 -0400

    Organize files better

--- docs/contracts/contractReload.js
+++ docs/contracts/contractReload.js
@@ -1,5 +1,4 @@
 d3.select("#reload").on("click", function() {
-    accountQuery(document.getElementById("number").value);
     contractQuery(document.getElementById("number").value);
     invokedQuery(document.getElementById("number").value, Math.round(new Date().getTime()) - 2929746000);
 });
