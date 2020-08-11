69b69cd5d92cf8de6a4f275c2b4b040ffe91cc74
PrathamRawat <prathamrawat815@gmail.com>
2020-08-11 13:45:34 -0400

    Add updated file

--- docs/operations/transactionsPerBlock.js
+++ docs/operations/transactionsPerBlock.js
@@ -97,12 +97,12 @@ let tphQuery = async function(date) {
         }) 
     } else {
         query = conseiljs.ConseilQueryBuilder.addFields(query, 'kind');
-        query = conseiljs.ConseilQueryBuilder.addFields(query, 'timestamp');
+        query = conseiljs.ConseilQueryBuilder.addFields(query, 'block_level');
         query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'kind', conseiljs.ConseilOperator.EQ, ['transaction']);
         query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'status', conseiljs.ConseilOperator.EQ, ['applied']);
         query = conseiljs.ConseilQueryBuilder.addPredicate(query, 'timestamp', conseiljs.ConseilOperator.BETWEEN, [date, new Date().getTime()]);
         query = conseiljs.ConseilQueryBuilder.addAggregationFunction(query, 'kind', conseiljs.ConseilFunction.count);
-        query = conseiljs.ConseilQueryBuilder.addOrdering(query, "timestamp", conseiljs.ConseilSortDirection.ASC);
+        query = conseiljs.ConseilQueryBuilder.addOrdering(query, "block_level", conseiljs.ConseilSortDirection.ASC);
         query = conseiljs.ConseilQueryBuilder.setLimit(query, 1000000000);
 
         const result = await conseiljs.ConseilDataClient.executeEntityQuery(conseilServer, 'tezos', conseilServer.network, 'operations', query);
@@ -111,46 +111,46 @@ let tphQuery = async function(date) {
 
         console.log(result);
 
-        label = [];
-        timestamps = [];
-        values = [];
-        data = [];
-
-        now = new Date().getTime()
-
-        for(var time = new Date(date).getTime(); time < now; time += 3600000) {
-            label.push(new Date(time));
-            timestamps.push(time);
-            values.push(0)
-        }
-
-        console.log(timestamps);
-        
-        for(var r = 0; r < result.length; r++) {
-            for(var t = label.length - 1; t > 0; t--) {
-                if(parseInt(result[r].timestamp) > parseInt(label[t].getTime())) {
-                    values[t] += parseInt(result[r].count_kind);
-                    break;
-                }
-            }
-        }  
-
-        for(var x = 0; x < values.length; x++) {
-            data.push({date : label[x].getTime(), values : parseInt(values[x])});
-        }
+        // label = [];
+        // timestamps = [];
+        // values = [];
+        // data = [];
+        //
+        // now = new Date().getTime()
+        //
+        // for(var time = new Date(date).getTime(); time < now; time += 3600000) {
+        //     label.push(new Date(time));
+        //     timestamps.push(time);
+        //     values.push(0)
+        // }
+        //
+        // console.log(timestamps);
+        //
+        // for(var r = 0; r < result.length; r++) {
+        //     for(var t = label.length - 1; t > 0; t--) {
+        //         if(parseInt(result[r].timestamp) > parseInt(label[t].getTime())) {
+        //             values[t] += parseInt(result[r].count_kind);
+        //             break;
+        //         }
+        //     }
+        // }
+        //
+        // for(var x = 0; x < values.length; x++) {
+        //     data.push({date : label[x].getTime(), values : parseInt(values[x])});
+        // }
 
         svg = d3.select("#transactionsPerHour");
 
         axis = d3.select("#tphAxis");
 
-        seperateAxisPrioritizedBarChartGenerator(500, 1200, svg, axis, data, "date", "values");
+        seperateAxisPrioritizedBarChartGenerator(500, 1200, svg, axis, result, "block_level", "count_kind");
 
         xTooltip = function(d, i) {
-            return new Date(timestamps[i])
+            return result[i].block_level
         }
 
         yTooltip = function(d, i) {
-            return d + " Transactions per Hour"
+            return d + " Transactions per Block "
         }
 
         barGraphFloatingTooltipGenerator(svg, xTooltip, yTooltip)
